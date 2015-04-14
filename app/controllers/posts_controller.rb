305e39da867b26cba34bdaa26428d5115cc4d05c
atoder <alexandert@gmail.com>
2015-04-14 12:14:14 -0700

    Posts now can be edited and deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -18,11 +19,30 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to :root
   end
 
   private
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def get_post
+      @post = Post.find(params[:id])
     end
 end
